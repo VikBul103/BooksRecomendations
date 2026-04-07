bb4a0075c03151947911012f188bb33590790ec5
VikBul103 <122801360+VikBul103@users.noreply.github.com>
2026-04-07 22:15:35 +0300

    Adding solution with Movie and Books Recomendation ML projects

---
+++ MovieRecommender1/BooksRecommendation/BooksRatingData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace BooksRecommendation
+{
+    public class BooksRating
+    {
+        [LoadColumn(0)]
+        public float user_id;
+        [LoadColumn(1)]
+        public float book_id;
+        [LoadColumn(2)]
+        public float Label;
+    }
+    public class BooksRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+    //public class BooksRating
+    //{
+    //    public int Id;
+    //    public string Name;
+    //    RatingDist1,pagesNumber,RatingDist4,RatingDistTotal,PublishMonth,PublishDay,Publisher,CountsOfReview,PublishYear,Language,Authors,Rating,RatingDist2,RatingDist5,ISBN,RatingDist3
+    //}
+}
---
+++ MovieRecommender1/MovieRecommender1/MovieRatingData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace MovieRecommender1
+{
+    public class MovieRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float movieId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+
+    public class BooksRating
+    {
+        public int Id;
+        public string Name;
+        RatingDist1,pagesNumber,RatingDist4,RatingDistTotal,PublishMonth,PublishDay,Publisher,CountsOfReview,PublishYear,Language,Authors,Rating,RatingDist2,RatingDist5,ISBN,RatingDist3
+    }
+}
