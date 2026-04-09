887295a3915d550a10d9d39a671b701e135eec26
VikBul103 <122801360+VikBul103@users.noreply.github.com>
2026-04-09 19:42:12 +0300

    adding "BookLibrary"

---
+++ MovieRecommender1/BookLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
+
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        MLContext mlContext = new MLContext();
+        var modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "BookRecommenderModel.zip");
+        DataViewSchema modelSchema;
+        ITransformer trainedModel = mlContext.Model.Load(modelPath, out modelSchema);
+    }
+
+    
+}
